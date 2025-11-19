7ebb1165949a5463c6ccbfd8a6f29f5655b59ab3
DCorn <161855667+DCornOnlineIT@users.noreply.github.com>
2025-11-19 15:43:08 -0600

    Create bandNameGenerator.py

---
+++ Day 1/bandNameGenerator.py
@@ -0,0 +1,19 @@
+# Band Name Generator
+# Day 1 Final Project
+
+print("Hello, welcome to the Band Name Generator")
+print("Please follow the prompts to generate a cool band name\n")
+
+# Ask the user for the city they grew up in and store it
+city = input("What city did you grow up in?\n")
+
+# Ask the user for their first pet's name and store it
+petName = input("What was the name of your first pet?\n")
+
+# Fun "loading" messages
+print("Generating Band Name Now...")
+print("Generation Completed!\n")
+
+# Final reveal
+print("Drumroll please...")
+print("Your new band name is: " + city + " " + petName)
